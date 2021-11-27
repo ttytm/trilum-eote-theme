3d9b08fc9f33cc2cb266196e720fa9d27ccc4d5f
tobealive <34311583+tobealive@users.noreply.github.com>
2021-11-27 19:53:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* Generic way to styling the nested file list */
+/* Generic way to style the nested file list */
 jQuery(document).ready(function($) {
     $('li').each((i, el) => $(el).children('span.fancytree-node').css('padding-left', 12 * $(el).parents('ul').length));
 });
