a92756907c20aa8be1de3948edfbd19010cba8c2
tobealive <34311583+tobealive@users.noreply.github.com>
2021-11-29 16:22:08 +0100

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-/* Generic way to style the nested file list */
-jQuery(document).ready(function($) {
-    $('li').each((i, el) => $(el).children('span.fancytree-node').css('padding-left', 12 * $(el).parents('ul').length));
-});
